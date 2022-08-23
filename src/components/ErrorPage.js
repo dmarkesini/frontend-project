40f9974f33a25520a6fed95ae6722ae1a19517c8
dmarkesini <markesinh@gmail.com>
2022-08-23 20:20:59 +0100

    added error handling

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,5 @@
+const ErrorPage = ({ errorMessage }) => {
+  return <div>error: {errorMessage}</div>;
+};
+
+export default ErrorPage;
--- src/components/Votes.js
+++ src/components/Votes.js
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import { patchArticleById } from "../api";
+import ErrorPage from "./ErrorPage";
 
 const Votes = ({ article }) => {
   const [upVote, setUpVote] = useState(false);
   const [downVote, setDownVote] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
   const vote = (voteValue) => {
-    patchArticleById(article.article.article_id, voteValue);
+    patchArticleById(article.article.article_id, voteValue).catch((err) => {
+      setError(true);
+      setErrorMessage(err.message);
+    });
     if (voteValue === +1) {
       setUpVote(true);
     } else {
@@ -15,29 +22,33 @@ const Votes = ({ article }) => {
 
   return (
     <>
-      <div className="articles-container_votes-buttons">
-        <p id="single-page_votes">Votes: {article.article.votes}</p>
-        <button
-          disabled={upVote}
-          onClick={() => vote(+1)}
-          style={{
-            backgroundColor: upVote && "rgb(255, 80, 10)",
-          }}
-          id="vote-button"
-        >
-          Upvote
-        </button>
-        <button
-          disabled={downVote}
-          onClick={() => vote(-1)}
-          style={{
-            backgroundColor: downVote && "rgb(255, 80, 10)",
-          }}
-          id="vote-button"
-        >
-          Downvote
-        </button>
-      </div>
+      {error ? (
+        <ErrorPage errorMessage={errorMessage} />
+      ) : (
+        <div className="articles-container_votes-buttons">
+          <p id="single-page_votes">Votes: {article.article.votes}</p>
+          <button
+            disabled={upVote}
+            onClick={() => vote(+1)}
+            style={{
+              backgroundColor: upVote && "rgb(255, 80, 10)",
+            }}
+            id="vote-button"
+          >
+            Upvote
+          </button>
+          <button
+            disabled={downVote}
+            onClick={() => vote(-1)}
+            style={{
+              backgroundColor: downVote && "rgb(255, 80, 10)",
+            }}
+            id="vote-button"
+          >
+            Downvote
+          </button>
+        </div>
+      )}
     </>
   );
 };
